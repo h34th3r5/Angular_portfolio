18660212ec6e3870cc8813d2bd43273e4773d542
h34th3r5 <heather.r.stump@gmail.com>
2019-08-06 16:59:51 -0500

    components figured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { GraphicDesignPortfolioComponent } from './graphic-design-portfolio/graphic-design-portfolio.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { WebPortfolioComponent } from './web-portfolio/web-portfolio.component';
+import { PhotographyPortfolioComponent } from './photography-portfolio/photography-portfolio.component';
 
 
 const routes: Routes = [
-  {path: 'portfolio'}
-  {path: }
+  {path: 'portfolio', component: PortfolioPageComponent},
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'photography', component: PhotographyPortfolioComponent},
+  {path: 'graphic-design', component: GraphicDesignPortfolioComponent},
+  {path: 'web-design', component: WebPortfolioComponent}
 ];
 
 @NgModule({
@@ -12,3 +22,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AboutPageComponent, ContactPageComponent, GraphicDesignPortfolioComponent, PortfolioPageComponent, WebPortfolioComponent, PhotographyPortfolioComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { PhotographyPortfolioComponent } from './photography-portfolio/photography-portfolio.component';
 import { GraphicDesignPortfolioComponent } from './graphic-design-portfolio/graphic-design-portfolio.component';
 import { WebPortfolioComponent } from './web-portfolio/web-portfolio.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    routingComponents,
     PortfolioPageComponent,
     ContactPageComponent,
     AboutPageComponent,
     PhotographyPortfolioComponent,
     GraphicDesignPortfolioComponent,
-    WebPortfolioComponent
+    WebPortfolioComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -33,9 +42,11 @@ import { WebPortfolioComponent } from './web-portfolio/web-portfolio.component';
     BrowserAnimationsModule,
     MatButtonModule, 
     MatCheckboxModule,
-    AppRoutingModule 
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
