85862a7fa6378b533c10ebadd422c7609296448d
h34th3r5 <heather.r.stump@gmail.com>
2019-08-30 18:10:05 -0500

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
+
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
