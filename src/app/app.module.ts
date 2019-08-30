0b7f7742e3e2ed78c056608aa3b9d41e5f844377
h34th3r5 <heather.r.stump@gmail.com>
2019-08-30 18:54:56 -0500

    comps deleted. new nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,10 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { PhotographyPortfolioComponent } from './photography-portfolio/photography-portfolio.component';
-import { GraphicDesignPortfolioComponent } from './graphic-design-portfolio/graphic-design-portfolio.component';
-import { WebPortfolioComponent } from './web-portfolio/web-portfolio.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 
 
@@ -27,15 +22,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   declarations: [
     AppComponent,
     routingComponents,
-    PortfolioPageComponent,
     ContactPageComponent,
-    AboutPageComponent,
-    PhotographyPortfolioComponent,
-    GraphicDesignPortfolioComponent,
-    WebPortfolioComponent,
     HeaderComponent,
-    FooterComponent,
-    NavbarComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
