4c79e7dc843d588ab2581f70e71f55da70c5a7fa
Stump <Heather.Stump@ENGILITY.COM>
2020-02-10 13:42:44 -0600

    redone changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 import { faInstagram, faFacebookSquare, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -16,6 +17,7 @@ export class HeaderComponent implements OnInit {
   faInstagram = faInstagram;
   faFacebookSquare = faFacebookSquare;
   faTwitter = faTwitter;
+  faEnvelope = faEnvelope;
 
   collapsed = true;
 
